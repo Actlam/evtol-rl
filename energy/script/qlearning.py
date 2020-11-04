e89e4250d9e182654ab4c31243b4895f56e2a22e
Actlam <stream.actlam@gmail.com>
2020-11-04 17:55:35 +0900

    [add]create base ros package

---
+++ energy/script/qlearning.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+
+current_state = State()
+
+def state_cb(msg):
+    global current_state
+    current_state = msg
+
+# Q学習エージェント
+class QLearningAgent:
+
+
+if __name__ == '__main__':
+    state_cb()
